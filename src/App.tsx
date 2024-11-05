52dee61df427b7c9ad0e8ab4aea067a3e53453af
Amil Musaevi <amilmusaevi00@gmail.com>
2024-11-05 16:54:38 +0400

    fixed some bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { routes } from './router'
 import { useRoutes } from 'react-router-dom'
 
-// import Header from './layout/header'
+import Header from './layout/header'
 
 const App = () => {
   const routing = useRoutes(routes)
   return (
     <>
-      {/* <Header/> */}
+      <Header/>
       {routing}
     </>
   )
