9e393be7e005972fe81cfa3eb34e9403775e9083
Amil Musaevi <amilmusaevi00@gmail.com>
2024-11-14 08:45:37 +0400

    created service

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { routes } from './router'
 import { useRoutes } from 'react-router-dom'
 
-import Header from './layout/header'
+
 
 const App = () => {
   const routing = useRoutes(routes)
   return (
     <>
-      <Header/>
       {routing}
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from './App'
-import "./styles/main.scss"
-createRoot(document.getElementById('root')!).render(
+import App from "./App";
+import "./styles/main.scss";
+
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>
+);
